2d9c66f4ff035e6df3302841eae8c2c5101645d3
MarcusLages <marcusvlages@gmail.com>
2026-05-11 18:23:32 -0700

    Separated unreachable macro into an unreachable.hpp header

--- src/primitives.hpp
+++ src/primitives.hpp
@@ -1,17 +1,6 @@
 #pragma once
+#include "unreachable.hpp"
 #include "mvmath.hpp"
-
-// Just used for a safer compilation
-#if defined(__cpp_lib_unreachable) && (__cpp_lib_unreachable >= 202202L)
-    #define _UNREACHABLE() std::unreachable()
-#elif defined(_MSC_VER)
-    #define _UNREACHABLE() __assume(0)
-#elif defined(__GNUC__) || defined(__clang__)
-    #define _UNREACHABLE() __builtin_unreachable()
-#else
-    #define _UNREACHABLE() abort()
-#endif
-
 
 namespace vkr {
     
---
+++ src/unreachable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Just used for a safer compilation to abort the application whenever
+// an unreachable code is reached (avoid exceptions)
+#ifndef _UNREACHABLE()
+#if defined(__cpp_lib_unreachable) && (__cpp_lib_unreachable >= 202202L)
+    #define _UNREACHABLE() std::unreachable()
+#elif defined(_MSC_VER)
+    #define _UNREACHABLE() __assume(0)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define _UNREACHABLE() __builtin_unreachable()
+#else
+    #define _UNREACHABLE() abort()
+#endif
+#endif
