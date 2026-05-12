9e48363fd54f0b5c1f0bf2ed9124752f29beb507
MarcusLages <marcusvlages@gmail.com>
2026-05-11 20:05:12 -0700

    Small hotfix in unreachable.hpp

--- src/unreachable.hpp
+++ src/unreachable.hpp
@@ -2,7 +2,7 @@
 
 // Just used for a safer compilation to abort the application whenever
 // an unreachable code is reached (avoid exceptions)
-#ifndef _UNREACHABLE()
+#ifndef _UNREACHABLE
 #if defined(__cpp_lib_unreachable) && (__cpp_lib_unreachable >= 202202L)
     #define _UNREACHABLE() std::unreachable()
 #elif defined(_MSC_VER)
